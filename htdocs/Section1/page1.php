4ac445b910a75cba318f12069940b4cc6ff27cb6
Zetona <lnwpalm@hotmail.com>
2015-08-24 11:12:02 +0700

    Lab 01

---
+++ htdocs/Section1/page1.php
@@ -0,0 +1,53 @@
+<html>
+<head><title>My First ITS 351</title></head>
+
+<link rel="stylesheet" type="text/css" href="page1.css"/>
+
+<body>
+
+<h1>ITS351</h1>
+<h3>ITS351</h3>
+<h6>ITS351</h6>
+
+<hr/>
+Database Programming Laboratory
+<font color="FF0000"> Database Programming Laboratory</font>
+Database Programming Laboratory
+<font color="FFFF00"> Database Programming Laboratory</font>
+Database Programming Laboratory
+<font color="0000FF"> Database Programming Laboratory</font>
+Database Programming Laboratory
+<font color="FFAAAA"> Database Programming Laboratory</font>
+Database Programming Laboratory
+
+<hr/>
+<table border="5">
+
+<tr>
+<td colspan="2" bgcolor="FF0000">1</td>
+<!--<td bgcolor="FF00FF">2</td>-->
+</tr>
+
+<tr>
+<td rowspan="2" bgcolor="FFFF00">3</td>
+<td bgcolor="FF6600">4</td>
+</tr>
+
+<tr>
+<td bgcolor="FF66FF">6</td>
+<!--<td bgcolor="FF6600">4</td>-->
+</tr>
+
+</table>
+
+<hr/>
+
+<div class="STYLE_ONE" id="ONE">HELLO <span>WORLD</span></div>
+<div class="STYLE_TWO">HELLO <span>WORLD</span></div>
+<div class="STYLE_THREE">HELLO <span>WORLD</span></div>
+<div class="STYLE_FOUR">HELLO <span>WORLD</span></div>
+
+<br/><br/><br/>
+
+</body>
+</html>
